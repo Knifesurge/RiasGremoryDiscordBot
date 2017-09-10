3307e4d1849f11ed6827cd2f3efd040dbcea935d
Knifesurge <carlos94563@gmail.com>
2017-09-10 01:55:34 -0400

    Remove run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp ".:bin:src.main.java.com.knifesurge.riasgremory:src.main.java.com.knifesurge.riasgremory.commands:src.main.java.com.knifesurge.riasgremory.commands.custom:src.main.java.com.knifesurge.riasgremory.commands.games:src.main.java.com.knifesurge.riasgremory.currency:src.main.java.com.knifesurge.riasgremory.games:src.main.java.com.knifesurge.riasgremory.games.chess:src.main.java.com.knifesurge.riasgremory.music:src.main.java.com.knifesurge.riasgremory.permissions:src.main.java.com.knifesurge.riasgremory.utils" com.knifesurge.riasgremory.RiasGremoryBot
